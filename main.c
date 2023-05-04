083373d205645ebf6802a019d693ae1b6ff6b8ce
yeongori <dusrjf14@gmail.com>
2023-05-04 12:06:06 +0900

    [feat] 파일 수정 날짜 비교 함수 작성 완료

--- main.c
+++ main.c
@@ -57,12 +57,53 @@ void blockcmp(){
     
 }
 
-//두 개의 파일 수정 날짜를 비교하는 함수 작성
-void datecmp(){
-    
+void datecmp(char *file1, char *file2) {
+	if (stat(file1, &stat1) != 0) {
+		printf("Failed to get information\n");
+    		return ;
+	}
+	if (stat(file2, &stat2) != 0) {
+		printf("Failed to get information\n");
+		return ;
+	}
+	
+	time1 = localtime(&stat1.st_mtime);
+	time2 = localtime(&stat2.st_mtime);
+
+	if (time1->tm_year > time2->tm_year)
+		printf("text1 is early\n");
+	else if (time1->tm_year < time2->tm_year)
+		printf("text2 is early\n");
+	else {
+		if (time1->tm_mon > time2->tm_mon)
+			printf("text1 is early\n");
+		else if (time1->tm_mon < time2->tm_mon)
+			printf("text2 is early\n");
+		else {
+			if (time1->tm_mday > time2->tm_mday)
+				printf("text1 is early\n");
+			else if (time1->tm_mday < time2->tm_mday)
+				printf("text2 is early\n");
+			else
+				printf("same time\n");
+		}
+	}
 }
 
-//두 개의 파일 수정 시간을 비교하는 함수 작성
-void timecmp(){
-    
+void timecmp(char *file1, char *file2) {
+	if (stat(file1, &stat1) != 0) {
+		printf("Failed to get information\n");
+		return ;
+	}
+	if (stat(file2, &stat2) != 0) {
+		printf("Failed to get information\n");
+		return ;
+	}
+
+	if (difftime(stat1.st_mtime, stat2.st_mtime) > 0)
+		printf("text1 is early\n");
+	else if (difftime(stat1.st_mtime, stat2.st_mtime) < 0)
+		printf("text2 is early\n");
+	else
+		printf("same time\n");	
 }
